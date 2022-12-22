21bc621b51f5dad25ede52b97e8d89d7c26f2423
NikFMatten <nfmatten@gmail.com>
2022-12-22 13:40:54 -0700

    refactor: changed max_length on fields from 20 to 50 to allow migrations

--- backend/songs/models.py
+++ backend/songs/models.py
@@ -3,11 +3,11 @@ from django.db import models
 # Create your models here.
 
 class Song(models.Model):
-    title = models.CharField(max_length = 20)
-    artist = models.CharField(max_length = 20)
-    album = models.CharField(max_length = 20)
+    title = models.CharField(max_length = 50)
+    artist = models.CharField(max_length = 50)
+    album = models.CharField(max_length = 50)
     release_date = models.DateField()
-    genre = models.CharField(max_length = 20)
+    genre = models.CharField(max_length = 50)
 
     def __str__(self):
         return self.title
